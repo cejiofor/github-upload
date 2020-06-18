e12c61b5c0adf0c98a809e9f5dcb4c435577b07a
cejiofor_intellij <cejiofor@gmail.com>
2020-06-18 12:24:36 -0500

    Initializing repository

---
+++ src/main/java/org/platform/selenium_basic_test/servlet/BasicServlet.java
@@ -0,0 +1,46 @@
+package org.platform.selenium_basic_test.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class BasicServlet
+ */
+@WebServlet("/BasicServlet")
+public class BasicServlet extends HttpServlet {
+	private static final long serialVersionUID = 3L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BasicServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/views/index.jsp");
+        requestDispatcher.forward(request, response);
+
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
